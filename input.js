1753075c38d49e9d793cc48d063b6eb9367c155d
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-05-20 23:11:41 -0400

    fixed some codes

--- input.js
+++ input.js
@@ -1,8 +1,7 @@
-// setup interface to handle user input from stdin
-// setupInput allows keyboard input and reaction
-// stdin = standard input
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,13 +12,33 @@ const setupInput = function () {
 // handleUserInput function will receive data from stdin (aka keyboard input)
 const handleUserInput = function () {
   stdin.on('data', (key) => {
-// \u0003 maps to ctrl+c input
-if (key === '\u0003') {
-  process.exit();
+
+    if (key === '\u0003') {
+      process.exit();
     };
+    if (key === '\u0077') {
+      connection.write('Move: UP');
+    }
+
+    if (key === '\u0061') {
+      connection.write('Move: LEFT');
+    }
+
+    if (key === '\u0073') {
+      connection.write('Move: DOWN');
+    }
+
+    if (key === '\u0064') {
+      connection.write('Move: RIGHT');
+    }
   });
 };
 
 setupInput();
 
 module.exports = { setupInput };
+
+
+// setup interface to handle user input from stdin
+// setupInput allows keyboard input and reaction
+// stdin = standard input
